85e29885f24ff3615951f4bc59e248c3252acc67
Maaz Imam <maazimam03@gmail.com>
2023-11-12 15:05:56 +0500

    changes

--- Graham Scan.py
+++ Graham Scan.py
@@ -1,57 +1,48 @@
 import matplotlib.pyplot as plt
-from math import atan2
+import numpy as np
 
 def graham_scan(points):
-    def orientation(p, q, r):
-        val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
-        if val == 0:
-            return 0
-        return 1 if val > 0 else 2
+    def cross_product(o, a, b):
+        return (a[1] - o[1]) * (b[0] - o[0]) - (a[0] - o[0]) * (b[1] - o[1])
 
-    def compare(p1, p2):
-        o = orientation(anchor, p1, p2)
-        if o == 0:
-            return -1 if distance(anchor, p1) < distance(anchor, p2) else 1
-        return 1 if o == 2 else -1
+    def angle_cmp(p1, p2):
+        return np.arctan2(p1[1] - anchor[1], p1[0] - anchor[0]) - np.arctan2(p2[1] - anchor[1], p2[0] - anchor[0])
 
-    def distance(p1, p2):
-        return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
-
-    if len(points) < 3:
-        print("Graham Scan requires at least three points.")
-        return
-
+    # Find the point with the lowest y-coordinate (and leftmost if ties)
     anchor = min(points, key=lambda p: (p[1], p[0]))
 
-    sorted_points = sorted(points, key=lambda p: (atan2(p[1] - anchor[1], p[0] - anchor[0]), p))
+    # Sort the points based on the polar angle from the anchor point
+    sorted_points = sorted(points, key=lambda p: angle_cmp(p, anchor))
 
+    # Initialize the convex hull with the anchor and the first two sorted points
     hull = [anchor, sorted_points[0], sorted_points[1]]
 
+    fig, ax = plt.subplots()
+
     for i in range(2, len(sorted_points)):
-        while len(hull) > 1 and orientation(hull[-2], hull[-1], sorted_points[i]) != 2:
+        # Pop the last point from the hull if a clockwise turn is encountered
+        while len(hull) > 1 and cross_product(hull[-2], hull[-1], sorted_points[i]) <= 0:
             hull.pop()
+
+        # Add the current point to the hull
         hull.append(sorted_points[i])
+
+        # Plot current state
+        ax.clear()
+        ax.scatter(*zip(*points), c='black', marker='o', label='Points')
+        ax.plot(*zip(*hull, hull[0]), color='r', linestyle='-', linewidth=2, label='Convex Hull')
+        ax.legend()
+        plt.title('Graham Scan - Step {}'.format(i + 1))
+        plt.pause(0.5)  # Adjust the pause time as needed
+
+    plt.show()
 
     return hull
 
-def plot_convex_hull(points, convex_hull):
-    x_points, y_points = zip(*points)
-    x_hull, y_hull = zip(*convex_hull)
+# Example usage
+if __name__ == "__main__":
+    # Replace this list with your set of coordinates
+    points = [(0, 0), (1, 1), (2, 2), (2, 0), (0, 2), (1, 0)]
 
-    plt.figure(figsize=(8, 8))
-    plt.scatter(x_points, y_points, color='blue', label='Points')
-    plt.plot(x_hull + (x_hull[0],), y_hull + (y_hull[0],), color='red', linestyle='-', linewidth=2, label='Convex Hull')
-    plt.title('Graham Scan Convex Hull')
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-
-# Example usage:
-points = [(0, 3), (1, 1), (2, 2), (4, 4), (0, 0), (1, 2), (3, 1), (3.5, 0.5)]
-convex_hull = graham_scan(points)
-
-print("Convex Hull:", convex_hull)
-
-plot_convex_hull(points, convex_hull)
+    # Run Graham Scan algorithm
+    convex_hull = graham_scan(points)
